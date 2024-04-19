bf66cecb04d6d2fd6a9a5ddcb1e9c0f89e1a0f32
Derek Fitzer <franzderek@gmail.com>
2024-04-19 09:00:56 -0400

    Initial Commit

---
+++ ClassGitSample/ClassGitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassGitSampleApp.swift
+//  ClassGitSample
+//
+//  Created by Derek Fitzer on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ClassGitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
